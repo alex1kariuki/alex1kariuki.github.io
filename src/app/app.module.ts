6c3d45a9f3f32e2c6c68f88fe6b881501c7e1289
alex.kariuki <alex.kariuki@gravity.earth>
2023-04-18 09:33:39 +0300

    Changed the issue with import of header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/components/header/header.component';
+import { HeaderComponent } from './shared/header/header.component';
 import { HomeComponent } from './core/home/home.component';
 import { ProjectsComponent } from './core/projects/projects.component';
 import { ContactComponent } from './core/contact/contact.component';
