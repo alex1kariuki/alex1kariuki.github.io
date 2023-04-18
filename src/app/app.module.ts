bc6d10390490d89825ba3e443662b9048d6a8cf9
alex.kariuki <alex.kariuki@gravity.earth>
2023-04-18 09:25:00 +0300

    Page setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { HomeComponent } from './core/home/home.component';
+import { ProjectsComponent } from './core/projects/projects.component';
+import { ContactComponent } from './core/contact/contact.component';
+import { SuccessComponent } from './core/success/success.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ProjectsComponent,
+    ContactComponent,
+    SuccessComponent
   ],
   imports: [
     BrowserModule,
